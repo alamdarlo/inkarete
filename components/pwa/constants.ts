9d37dc93b910404766350f1070c235db0fdc4758
alamdarlo <34776845+alamdarlo@users.noreply.github.com>
2026-08-20 13:48:36 +0330

    fix: track PWA installed state separately

--- components/pwa/constants.ts
+++ components/pwa/constants.ts
@@ -1,4 +1,5 @@
 export const PWA_INSTALL_SEEN_KEY = "pwa-install-seen";
+export const PWA_INSTALLED_KEY = "pwa-installed";
 
 export const APP_NAME = "این کارته";
 export const APP_DESCRIPTION = "برنامه را نصب کنید تا سریع‌تر و حتی بدون اینترنت از آن استفاده کنید.";
